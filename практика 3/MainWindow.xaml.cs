cbfd5796d0368b5443cae633dc426eb24264b4b6
Simens-code <semn.kuznecov.06@gmail.com>
2024-09-22 14:26:37 +0500

    Добавьте файлы проекта.

---
+++ практика 3/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace практика_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Car> cars {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            cars = new List<Car>();
+            CarsDataGrid.ItemsSource = cars;
+        }
+
+        private void AddCarButton_Click(object sender, RoutedEventArgs e)
+        {
+            string CarMake = CarMakeTextBox.Text;
+            string Model = ModelTextBox.Text;
+            string YearOfManufacture = YearOfManufactureTextBox.Text;
+
+            Car newCar = new Car
+            {
+                CarMake = CarMake,
+                Model = Model,
+                YearOfManufacture = YearOfManufacture
+            };
+
+            cars.Add(newCar);
+            CarMakeTextBox.Clear();
+            ModelTextBox.Clear();
+            YearOfManufactureTextBox.Clear();
+        }
+    }
+    public class Car
+    {
+        public string CarMake { get; set; }
+        public string Model { get; set; }
+        public string YearOfManufacture { get; set; }
+    }
+
+}
